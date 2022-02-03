7c4ea0aab71e4d8e1eda5da54c39215d71a1f5aa
Etin-osa <frederickade15@gmail.com>
2022-02-03 15:03:42 +0100

    Intialize firebase db to the project
    Just finished learning how to initialize firebase into the project, Sorry it happening this late i had to eat and bath and i haven't used firebase Nosql before

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+// import config from '../firebase/config.js';
+import React from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from 'firebase/analytics';
+import config from '../firebase/config'
+
 export default function Home () {
+  React.useEffect(() => {
+    const app = initializeApp(config);
+    const analytics = getAnalytics(app);
+    console.log({ app, analytics })
+  })
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       ESTUDIO CACTUS VISUALIZER TEST
